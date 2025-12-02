d2ddbe41d501e79c96e675e287a7a496a8d17109
Sophia Rubens <116817240+sophiarubens@users.noreply.github.com>
2025-12-02 15:32:37 -0500

    describing functionality

--- cosmo_distances.py
+++ cosmo_distances.py
@@ -11,6 +11,7 @@ nu21=1420.405751768 # MHz
 pc=30856775814914000 # m
 Mpc=pc*1e6
 
+# helpful elsewhere in this module
 def comoving_dist_arg(z,Omegam=Omegam_Planck18,OmegaLambda=OmegaLambda_Planck18): # this is 1/ E(z)
     return 1/np.sqrt(Omegam*(1+z)**3+OmegaLambda)
 
@@ -18,6 +19,7 @@ def comoving_distance(z,H0=H0_Planck18,Omegam=Omegam_Planck18,OmegaLambda=OmegaL
     integral,_=quad(comoving_dist_arg,0,z,args=(Omegam,OmegaLambda,))
     return (c*integral)/(H0*1000)
 
+# typical trivial conversions
 def freq2z(nu_rest,nu_obs):
     return nu_rest/nu_obs-1
 
@@ -31,6 +33,10 @@ def z2wl(lambda_rest,z):
     return lambda_rest*(z+1)
 
 def kpar(nu_ctr,chan_width,N_chan,H0=H0_Planck18):
+    """
+    not "pure theory" kparallel values
+    (relies on line-of-sight details of your survey)
+    """
     prefac=1e3*twopi*H0*nu21/c # 1e3 to account for units of H0/c ... assumes nu21 and chan_width have the same units
     z_ctr=freq2z(nu21,nu_ctr)
     Ez=1/comoving_dist_arg(z_ctr)
@@ -40,6 +46,10 @@ def kpar(nu_ctr,chan_width,N_chan,H0=H0_Planck18):
     return np.linspace(kparmin,kparmax,N_chan) # evaluating at the z of the central freq of the survey (trusting slow variation...)
 
 def kperp(nu_ctr,N_baselines,bmin,bmax):
+    """
+    not "pure theory" kperp values
+    (relies on sky plane details of your survey)
+    """
     Dc=comoving_distance(freq2z(nu21,nu_ctr)) # evaluating at the z of the central freq of the survey (trusting slow variation, once again)
     prefac=twopi*nu21*1e6/(c*Dc)
     kperpmin=prefac*bmin
@@ -47,6 +57,9 @@ def kperp(nu_ctr,N_baselines,bmin,bmax):
     return np.linspace(kperpmin,kperpmax,N_baselines) # in Mpc^{-1} as long as I use nu21 in MHz, c in m s^{-1}, and Dc in Mpc^{-1}
 
 def wedge_kpar(nu_ctr,kperp,H0=H0_Planck18,nu_rest=nu21):
+    """
+    for some kperps of interest, which kparallels will the interferometer smear the wedge up to?
+    """
     z=freq2z(nu_rest,nu_ctr)
     E=1/comoving_dist_arg(z)
     Dc=comoving_distance(z)
