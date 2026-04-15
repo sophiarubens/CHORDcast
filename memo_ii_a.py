832e06ac2efad794fb896e29cf41db723d16c558
Sophia Rubens <116817240+sophiarubens@users.noreply.github.com>
2026-04-15 18:11:07 -0400

    Memo IIa2

---
+++ memo_ii_a.py
@@ -0,0 +1,12 @@
+from forecasting_pipeline import *
+
+Nft=[4]
+Npt=[1]
+power_comparison_plots(redo_window_calc=True, redo_box_calc=True,
+                       mode="pathfinder", nu_ctr=600.*u.MHz, epsxy=0.1,
+                       frac_tol_conv=0.05, categ="PA", # categ is manual/PA/CST, beam_type is either Gaussian (for PA) or manual (for CST) # frac_tol_conv=0.005 for Fir
+                       N_fidu_types=Nft, N_pert_types=Npt, 
+                       N_pbws_pert=30, per_channel_systematic=None,
+                       PA_dist="column", plot_qty="P",
+                            
+                       layer_foregrounds=True, pointing_error=[12.,0.,-30.])
