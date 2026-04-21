2b2c5509d03c60787572131250f166f334d9e9d8
Sophia Rubens <116817240+sophiarubens@users.noreply.github.com>
2026-04-21 09:15:40 -0400

    cleanup and prep for memo IIb1

--- memo_ii_b.py
+++ memo_ii_b.py
@@ -1,15 +1,17 @@
 from forecasting_pipeline import *
 
-CST_dir="/Users/sophiarubens/Downloads/research/code/pipeline/CST_beams/CHORD_CST_600/"
+CST_dir="/Users/sophiarubens/Downloads/research/code/pipeline/CST_beams/CHORD_CST_600/" # local
+# CST_dir="/home/sophiaru/scratch/pipeline/CST/" # Fir
 ff="farfield_(f="
+pointingerrs=[ [1.5,-0.7,0.4],   [0.,2.6,-1.5],   [-0.9,0.1,1.2],  [0.9,-0.3,1.5], [2.7,-0.6,0.],
+               [-0.1,0.1,0.2],   [-1.1,0.9,-1.2], [0.2,-0.2,-0.3], [-0.1,0.,0.1],  [5.,-0.1,1.8],
+               [-0.3,-0.2,-0.4], [0.,0.,-0.3],    [2.1,0.1,-0.5],  [3.2,-0.1,2.3], [4.4,-2.1,-0.7] ]
 
-# per-antenna Gaussian: Nft has all elements >=1; Npt has all elements >=0 [array-like of options]
-# per-antenna CST: N_PA_CST_types >=1 [!!scalar. makes more sense to handle this way, even if the philosophy is different from ^]
-
-power_comparison_plots(redo_window_calc=True, redo_box_calc=True,
+power_comparison_plots(redo_window_calc=True, redo_box_calc=True, alr_imp_CST=True,
                        mode="pathfinder", nu_ctr=600.*u.MHz, layer_foregrounds=True,
-                       frac_tol_conv=0.05, categ="PA",                                # categ is manual/PA/CST, beam_type is either Gaussian (for PA) or manual (for CST) # frac_tol_conv=0.005 for Fir
-                       N_PA_CST_types=3, pointing_error=[1.5,-0.7,0.4],
+                       frac_tol_conv=0.1, categ="PACST", PA_dist="frame",             # categ is manual/PA/CST/PACST, beam_type is either Gaussian (for PA) or manual (for CST) # frac_tol_conv=0.005 for Fir
+                       N_PA_CST_types=6, pointing_errors=pointingerrs[:6],
                        CST_lo=0.58*u.GHz,CST_hi=0.62*u.GHz,CST_deltanu=2e-4*u.GHz,    # 580-620 MHz = 0.58-0.62 GHz; 200 kHz = 0.2 MHz = 2e-4 GHz
                        beam_sim_directory=CST_dir,
-                       CST_f_head_fidu="fiducial/"+ff,CST_f_head_real="fiducial/"+ff,CST_f_head_thgt="feed_tilt/"+ff)
+                       CST_f_head_fidu="fiducial/"+ff,CST_f_head_real="fiducial/"+ff,CST_f_head_thgt="feed_tilt/"+ff) # local
+                    #    CST_f_head_fidu="fidu/"+ff,CST_f_head_real="fidu/"+ff,CST_f_head_thgt="syst/"+ff) # Fir
